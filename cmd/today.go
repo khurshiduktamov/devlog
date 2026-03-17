a075f84de7053fc7be9b5222a66b554d650ffc8e
khurshiduktamov <xurshid3777@gmail.com>
2026-03-17 02:47:04 -0400

    Implement devlog today command

--- cmd/today.go
+++ cmd/today.go
@@ -1,3 +1,40 @@
 package cmd
 
-// today command will be implemented in Step 7.
+import (
+	"fmt"
+
+	"github.com/khurshiduktamov/devlog/internal/activity"
+	"github.com/khurshiduktamov/devlog/internal/git"
+	"github.com/spf13/cobra"
+)
+
+var todayCmd = &cobra.Command{
+	Use:   "today",
+	Short: "Show today's git commits",
+	RunE:  runToday,
+}
+
+func init() {
+	rootCmd.AddCommand(todayCmd)
+}
+
+func runToday(cmd *cobra.Command, args []string) error {
+	commits, err := git.GetCommitsSince("24 hours ago")
+	if err != nil {
+		return fmt.Errorf("failed to fetch commits: %w", err)
+	}
+
+	activities := activity.FromCommits(commits)
+
+	if len(activities) == 0 {
+		fmt.Println("No activity found in the last 24 hours.")
+		return nil
+	}
+
+	fmt.Println("Today:")
+	for _, a := range activities {
+		fmt.Printf("  • %s\n", a.Message)
+	}
+
+	return nil
+}
