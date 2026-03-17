ed6875d760e52090abff773c175cec226a6d11df
khurshiduktamov <xurshid3777@gmail.com>
2026-03-17 02:55:12 -0400

    implemented adding manual notes

--- cmd/note.go
+++ cmd/note.go
@@ -1,3 +1,30 @@
 package cmd
 
-// note command will be implemented in Step 8.
+import (
+	"fmt"
+
+	"github.com/khurshiduktamov/devlog/internal/notes"
+	"github.com/spf13/cobra"
+)
+
+var noteCmd = &cobra.Command{
+	Use:   "note [message]",
+	Short: "Save a manual work note",
+	Args:  cobra.ExactArgs(1),
+	RunE:  runNote,
+}
+
+func init() {
+	rootCmd.AddCommand(noteCmd)
+}
+
+func runNote(cmd *cobra.Command, args []string) error {
+	message := args[0]
+
+	if err := notes.AddNote(message); err != nil {
+		return fmt.Errorf("failed to save note: %w", err)
+	}
+
+	fmt.Println("Note saved.")
+	return nil
+}
